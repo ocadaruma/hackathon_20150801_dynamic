70a44424a48674c85f79e1ec0599aa03fa765e3d
ocadaruma <ocadaruma@gmail.com>
2015-08-02 21:44:23 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'songs/top'
-  # get 'songs/search'
 
   post 'songs/result'
 
@@ -8,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'songs#top'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
